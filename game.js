9714be0dae7a5cbf424c57d3e54d749f1db6aa6e
ET <timofeyefimenko.07@mail.ru>
2026-05-27 09:41:07 +0700

    gjdeg

--- game.js
+++ game.js
@@ -41,7 +41,7 @@ const showRecords = () => {
             <td>${record.date}</td>
         </tr>`
     })
-    const html = `<table border="1" cellpadding="5"><tr><th>#</th><th>Имя</th><th>Попытки</th><th>Дата</th></tr>${tableRows}</table>`
+    const html = `<table border="1" cellpadding="5"><tr><th>#</th><th>Имя</th><th>Попытки</th><th>Дата</th></tr>${tableRows}<tr>`
     recordsDiv.innerHTML = html
 }
 
